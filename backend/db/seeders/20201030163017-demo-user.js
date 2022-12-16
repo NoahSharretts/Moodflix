0378fb62fb4693ec2332e49861f5110555bd8be1
Noah <garciasharrettsnoah@gmail.com>
2022-12-16 15:12:18 -0800

    working on frontend, splash page made, working on signup page

--- backend/db/seeders/20201030163017-demo-user.js
+++ backend/db/seeders/20201030163017-demo-user.js
@@ -29,7 +29,7 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      username: { [Op.in]: ['Demo', 'FakeUser1', 'FakeUser2'] }
     }, {});
   }
 };
--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,18 +1,103 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Redirect } from 'react-router-dom'
 import './Splash.css'
-import { Link } from 'react-router-dom';
 
-const Splash = () => {
-    return (
-        <>
-          <div className="main-div">
-          <h1 className = "splash-main-header"> Moodflix </h1>
-          <h2 className ="splash-sub-header"> Why not review media with more media? </h2>
-          <button className="splash-button">
-          <Link to="/login" className="open">Open Moodflix</Link>
-          </button>
+function Splash() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password }))
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      });
+  };
+
+  const handleDemo = () => {
+
+    dispatch(sessionActions.login({ credential: "demo@user.io", password: "password" }))
+
+    return
+  }
+
+  return (
+    <>
+      <div className='splash-page'>
+        <div className='tagline-wrapper'>
+          <span>
+            Moodflix - Reviewing media with media
+          </span>
+        </div>
+        <div className='splash-login'>
+          <div className='form-wrapper'>
+            <div className='title-wrapper'>
+              <img className='logo' src="https://i.imgur.com/g0RHOR6.png" alt="" />
+            </div>
+            <form className="login-form" onSubmit={handleSubmit}>
+              <ul>
+                {errors.map((error, idx) => (
+                  <li key={idx}>{error}</li>
+                ))}
+              </ul>
+              <input
+                className="login-form-input"
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+                placeholder="Email"
+              />
+              <input
+                className="login-form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder="Password"
+              />
+              <button className="login-btn" type="submit">Log In</button>
+              <button className="login-btn" onClick={handleDemo}>Demo User</button>
+            </form>
           </div>
-        </>
-    )
+          <div className="sign-up-form">
+            <p>
+              {" "}
+              Don't have an account?&nbsp;
+            <Link className="sign-up btn" to="/signup">Sign-up</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="social-links">
+          <p> Noah Garcia-Sharretts</p>
+          <a className="icons" href="https://github.com/NoahSharretts">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+              alt="githubLogo"
+            />
+          </a>
+          <a className="icons" href="https://www.linkedin.com/in/noah-garcia-sharretts-7ab735208/">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+              alt="linkedInLogo"
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Splash;
